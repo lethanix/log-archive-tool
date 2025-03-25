8b2072470c4a8d0f26930f60784d43f93831177b
lethanix <developer@lethanix.com>
2025-03-24 22:14:47 -0600

    Add compression and saving logic

--- log-archive.sh
+++ log-archive.sh
@@ -2,10 +2,12 @@
 #
 # Script to compress and store logs in a new directory
 
+# Name of the output folder 
+OUTPUT="archout"
+
 # ****************************
 # Verify input of the script
 # ****************************
-
 ## Verify the number of arguments 
 if [ $# -eq 0 ] && [ $# -lt 2 ]; then
     echo Missing argument: Directory is not provided $1 1>&2
@@ -26,7 +28,25 @@ fi
 
 
 # ****************************
-# Compress directory
+# Verify output of the script
 # ****************************
+## Find the output folder in the current directory
+## If not found, create it
+OUTPUT_FOUND=$(find . -maxdepth 1 -type d -name $OUTPUT) 
+if [ -n "$OUTPUT_FOUND" ]; then
+    echo Output folder located $OUTPUT_FOUND
+else 
+    echo "Creating output folder '$OUTPUT'"
+    mkdir $OUTPUT
+fi
 
+# ******************************
+# Compress & save the directory
+# ******************************
+## Get the date and time 
+DATE=$(date -u +%Y%m%d_%H%M%S%Z)
+LOCATION="$OUTPUT/logs_archive_$DATE"
+tar czf "$LOCATION.tar.gz" $1
 
+## Display location of archive
+echo Directory archived at: $LOCATION
